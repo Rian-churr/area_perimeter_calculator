0535f0cc9479c7978fc656f3736ad9d5afdb7a30
Rian Kiyas <99842848+Rian-churr@users.noreply.github.com>
2022-04-04 21:29:50 +0000

    Made basic functioning calculator

--- main.py
+++ main.py
@@ -13,46 +13,42 @@ from tkinter import ttk
 
 
 # ____________   FUNCTIONS ________________
-def find_perimeter(l,w,):
-    # find the perimeter ( Total of all lengths )
-    perimeter = lengths.width((lengths.width)<(l,w))
-    return perimeter
-def display_calc_perimeter(perimeter):
-  tbox_perimeter.config(state='normal')
-
-   # gets the three entries
-  l=int(input("Length : "))
-w=int(input("Width : "))
-area=l*w
-perimeter=2*(l+w)
-print("Area of Rectangle : ",area)
-print("Perimeter of Rectangle : ",perimeter)
-
-  # find the perimeter ( total of dimensions ) perimeter = lengths.width<(l,w))
-tbox_age.config(state='normal')
-   #perimeter calculated is inserted into the text box after clearing the previous info in the textbox. 
-tbox_perimeter.delete('1.0', tk.END)
-tbox_perimeter.insert(tk.END,perimeter)
-tbox_age.config(state='disabled')
-
-
-
-def validation():
-  # gets the three entries
-  l = e_lenght.get()
-  w = e_width.current()
-  
-  msg = ''
-
-# ____________   MAIN  ________________
- # Labels for Heading and Subheadng of GUI
-lb_heading = tk.Label(window,text="Area and Perimeter calculator!",font=("Arial", 20),fg="black",bg="#F7DC6F")
-lb_subheading = tk.Label(window,font=("Arial",12),text="Enter your dimensions which includes the width and length.",fg="black",bg="#F7DC6F")
+# -*- coding: utf-8 -*-
+import tkinter as tk
  
-  
+def calculate():
  
-  # Labels for date, month and year
-lb_date = tk.Label(window,text = "width: ",font=('Arial',12,"bold"),fg="darkgreen",bg="#F7DC6F"
-lb_month = tk.Label(window,text = "length:",font= ("Arial",12,"bold"),fg="darkgreen",bg="#F7DC6F)
-lb_year = tk.Label(window,text = "perimeter: ",font=('Arial',12,"bold"),fg="darkgreen",bg="#F7DC6F")
-tbox_perimeter.tk.Label()
+  if valRadio.get() == 1:
+    res = (int(e1.get()) + int(e2.get())) * 2 #Perimeter
+  elif valRadio.get() == 2:
+    res = int(e1.get()) * int(e2.get()) #Area
+  else:
+    res ="check radio button"
+  myText.set(res)
+ 
+ 
+root = tk.Tk()
+root.title("Calculator")     # Add a title
+ 
+valRadio = tk.IntVar()
+myText=tk.StringVar()
+e1 =tk.StringVar()
+e2 =tk.StringVar()
+ 
+tk.Label(root, 
+    text="""Area and Perimeter Calculator""").grid(row = 0, column = 0)
+tk.Label( text="Length :" ).grid(row=1,column = 0)
+tk.Label( text="Width :").grid(row=2,column=0)
+result=tk.Label(text="(result)", textvariable=myText).grid(row=5,column=0)
+ 
+r1 = tk.Radiobutton(text="Perimeter",
+      variable=valRadio, value=1).grid(row=3, column=0)
+r2 = tk.Radiobutton(text="Area",
+      variable=valRadio, value=2).grid(row=3, column=1)
+ 
+tk.Entry(textvariable = e1).grid(row=1, column=1)
+tk.Entry(textvariable = e2).grid(row=2, column=1)
+ 
+b = tk.Button(text="Calculate", command=calculate).grid(row=1, column=3)
+ 
+root.mainloop()
